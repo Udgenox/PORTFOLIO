4db797ddeb50fad6aecd588ec5f3e5fa6d1ed234
Udgenox <udgenkovaly@gmail.com>
2025-07-04 17:08:08 +0300

    Main

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -1,17 +1,21 @@
 import styled from "styled-components";
 import photo from '../../../assets/Ava.webp'
+import {FlexWrapper} from "../../../components/FlexWrapper.tsx";
+
 
 export const Main = () => {
     return (
-        <div>
-            <div>
-                <span>Hi there</span>
-                <h2>I am Svetlana Dyablo</h2>
-                <h1>A Web Developer.</h1>
-            </div>
+        <StyledMain>
+            <FlexWrapper align={"center"} justify={"space-around"} >
+                <div>
+                    <span>Hi there</span>
+                    <Name>I am Svetlana Dyablo</Name>
+                    <MainTitle>A Web Developer.</MainTitle>
+                </div>
 
-            <Photo src={photo} alt=""/>
-        </div>
+                <Photo src={photo} alt=""/>
+            </FlexWrapper>
+        </StyledMain>
     );
 };
 
@@ -22,3 +26,17 @@ const Photo = styled.img`
     height: 430px;
     object-fit: cover;
 `
+
+const StyledMain = styled.div`
+    min-height: 100vh;
+    background-color: antiquewhite;
+`
+
+const MainTitle = styled.h1`
+    
+`
+
+const Name = styled.h2`
+
+`
+
