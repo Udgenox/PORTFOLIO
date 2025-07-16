c5cb6e19ec1dd6ea2e60843b8ef2780a9281348d
Udgenox <udgenkovaly@gmail.com>
2025-07-16 16:58:54 +0300

    Main part 2

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -1,20 +1,28 @@
 import styled from "styled-components";
 import photo from '../../../assets/Ava.webp'
 import {FlexWrapper} from "../../../components/FlexWrapper.tsx";
+import {Container} from "../../../components/Container";
+import {theme} from "../../../styles/Theme.ts";
+
 
 
 export const Main = () => {
     return (
         <StyledMain>
-            <FlexWrapper align={"center"} justify={"space-around"} >
-                <div>
-                    <span>Hi there</span>
-                    <Name>I am Svetlana Dyablo</Name>
-                    <MainTitle>A Web Developer.</MainTitle>
-                </div>
+            <Container>
+                <FlexWrapper align={"center"} justify={"space-between"} >
+                    <div>
+                        <SmallText>Hi there</SmallText>
+                        <Name>I am <span>Evgeniy Kovalev</span> </Name>
+                        <MainTitle>A Web Developer.</MainTitle>
+                    </div>
+                    <PhotoWrapper>
+                        <Photo src={photo} alt=""/>
+                    </PhotoWrapper>
 
-                <Photo src={photo} alt=""/>
-            </FlexWrapper>
+                </FlexWrapper>
+            </Container>
+
         </StyledMain>
     );
 };
@@ -29,14 +37,68 @@ const Photo = styled.img`
 
 const StyledMain = styled.section`
     min-height: 100vh;
-    background-color: antiquewhite;
+    background-color: #ddd;
+    display: flex;
 `
 
 const MainTitle = styled.h1`
-    
+    li
+    font-weight: 400;
+    font-style: Regular;
+    font-size: 27px;
+    leading-trim: NONE;
+    line-height: 100%;
+    letter-spacing: 0%;
+
 `
 
 const Name = styled.h2`
-
+    font-family: 'Josefin Sans', sans-serif;
+    font-weight: 700;
+    font-size: 50px;
+    letter-spacing: 0.05em;
+    margin: 10px 0;
+    
+    span {
+        position: relative;
+        z-index: 0;
+        
+        &::before {
+            content: "";
+            display: inline-block;
+            width: 100%;
+            height: 20px;
+            background-color: ${theme.colors.accent}; 
+            
+            position: absolute;
+            bottom: 0;
+            z-index: -1;
+        }
+    }
 `
+
+const SmallText = styled.h2`
+    /* Hi There */
+    font-weight: 400;
+    font-size: 14px;
+`
+
+const PhotoWrapper = styled.div`
+    position: relative;
+    z-index: 0;
+    
+    &::before {
+        content: "";
+        position: absolute;
+        width: 360px;
+        height: 470px;
+        border: 5px solid ${theme.colors.accent};
+        top: -24px;
+        left: 24px;
+        z-index: -1;
+        
+    }
+`
+
+
 
