6542d959b4444ba186e7418813819e0e31093b0a
Udgenox <udgenkovaly@gmail.com>
2025-07-20 16:08:34 +0300

    Slogan part 2

--- src/layout/sections/slogan/Slogan.tsx
+++ src/layout/sections/slogan/Slogan.tsx
@@ -1,13 +1,20 @@
 import styled from "styled-components";
 import {SectionTitle} from "../../../components/SectionTitle.tsx";
 import {Button} from "../../../components/Button.tsx";
+import {Container} from "../../../components/Container.ts";
+import {FlexWrapper} from "../../../components/FlexWrapper.tsx";
 
 
 export const Slogan = () => {
     return (
         <StyledSlogan>
-            <SectionTitle> I Am Available for Freelance</SectionTitle>
-            <Button>Hire me</Button>
+            <Container>
+                <FlexWrapper direction="column" align={"center"}>
+                    <SectionTitle> I Am Available for Freelance</SectionTitle>
+                    <Button>Hire me</Button>
+                </FlexWrapper>
+            </Container>
+
         </StyledSlogan>
     );
 };
@@ -16,6 +23,5 @@ const StyledSlogan = styled.section`
     min-height: 30vh;
     background-color: rgba(20,195,173,0.53);
     display: flex;
-    flex-direction: column;
     align-items: center;
 `
