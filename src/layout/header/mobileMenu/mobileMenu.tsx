fa802e3d00ba75ad26814abed8876f4b123be956
Udgenox <udgenkovaly@gmail.com>
2025-07-23 20:01:59 +0300

    Main part 3

--- src/layout/header/mobileMenu/mobileMenu.tsx
+++ src/layout/header/mobileMenu/mobileMenu.tsx
@@ -7,11 +7,11 @@ import {theme} from "../../../styles/Theme.ts";
 export const MobileMenu = (props: {menuItems: Array<string>}) => {
     return (
         <StyledMobileMenu>
-            <BurgerButton isOpen={true}>
+            <BurgerButton isOpen={false}>
                 <span></span>
             </BurgerButton>
 
-            <MobileMenuPopup isOpen={true}>
+            <MobileMenuPopup isOpen={false}>
                 <ul>
                     {props.menuItems.map((item, index)  =>{
                         return <ListItem key={index}>
