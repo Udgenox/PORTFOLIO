b80be48f9168f4338aad1a78262bc2490505fdbb
Udgenox <udgenkovaly@gmail.com>
2025-07-24 20:12:31 +0300

    Skills part 3

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -1,11 +1,14 @@
 import styled from "styled-components";
 import {theme} from "../styles/Theme.ts";
+import {font} from "../styles/Common.ts";
 
 export const SectionTitle = styled.h2`
+    ${font({family:"'Josefin Sans', sans-serif", weight: 600, Fmin:30, Fmax:36})}
+    
     text-align: center;
-    font-family: 'Josefin Sans', sans-serif;
-    font-weight: 600;
-    font-size: 36px;
+    //font-family: 'Josefin Sans', sans-serif;
+    //font-weight: 600;
+    //font-size: 36px;
     letter-spacing: 5px;
     margin-bottom: 90px;
     
@@ -22,5 +25,9 @@ export const SectionTitle = styled.h2`
         left: 50%;
         bottom: -30px;
         transform: translateX(-50%);
+        
+        @media ${theme.media.mobile} {
+            bottom: -24px;
+        }
     }
 `;
