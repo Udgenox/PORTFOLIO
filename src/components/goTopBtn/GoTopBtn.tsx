a1b61e5ac0dd1645e12d2cbc7f5866c0fbcb19ac
Udgenox <udgenkovaly@gmail.com>
2025-08-14 11:24:53 +0300

    Кнопка GoTop

---
+++ src/components/goTopBtn/GoTopBtn.tsx
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+import {Icon} from "../icon/Icon.tsx";
+import {animateScroll as scroll} from 'react-scroll'
+import {useEffect, useState} from "react";
+
+export const GoTopBtn = () => {
+
+    const [showBtn, setshowBtn] = useState(false)
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 200) {
+                setshowBtn(true)
+            } else {
+                setshowBtn(false)
+            }
+        });
+    }, [])
+
+    return (
+
+        <>
+            {showBtn && (
+                <StyledGoTopBtn onClick={()=>{scroll.scrollToTop()}}>
+                    <Icon iconId={"arrowGoTop"} height={'15'} width={'16'} viewBox={'0 0 16 15'} />
+                </StyledGoTopBtn>
+            )}
+        </>
+    );
+};
+
+const StyledGoTopBtn = styled.button`
+    background-color: rgba(0,0,0,0.3);
+    padding: 8px;
+    position: fixed;
+    right: 30px;
+    bottom: 30px;
+`
