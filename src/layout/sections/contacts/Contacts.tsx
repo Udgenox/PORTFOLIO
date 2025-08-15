cc7216d2ad648dd4fa0894345173870e028fcb0c
Udgenox <udgenkovaly@gmail.com>
2025-08-15 16:45:22 +0300

    Отправка данных из формы на почту: Emailjs

--- src/layout/sections/contacts/Contacts.tsx
+++ src/layout/sections/contacts/Contacts.tsx
@@ -1,23 +1,47 @@
-
 import {SectionTitle} from "../../../components/SectionTitle.ts";
 import {Button} from "../../../components/Button.ts";
 import {Container} from "../../../components/Container.ts";
-
-import {S} from './Contacts_Styles.ts'
+import {S} from './Contacts_Styles.ts';
+import emailjs from '@emailjs/browser';
+import {ElementRef, useRef} from "react";
 
 
 
 export const Contact : React.FC = () => {
+    const form = useRef<ElementRef<'form'>>(null);
+
+    const sendEmail = (e: any) => {
+        e.preventDefault();
+
+        if (!form.current) return
+
+        emailjs
+            .sendForm('service_mkaw9mr', 'template_leese02', form.current, {
+                publicKey: 'LFhj0SGc0B5hdkgrh',
+            })
+            .then(
+                () => {
+                    console.log('SUCCESS!');
+                },
+                (error) => {
+                    console.log('FAILED...', error.text);
+                },
+
+            );
+        e.target.reset();
+    };
+
     return (
         <S.Contacts id={'contact'}>
             <Container>
                 <SectionTitle>
                     Contact
                 </SectionTitle>
-                <S.Form>
-                    <S.Field placeholder={"name"}/>
-                    <S.Field placeholder={"subject"}/>
-                    <S.Field placeholder={"message"} as={"textarea"}/>
+                <S.Form ref={form} onSubmit={sendEmail}>
+                    <S.Field required placeholder={"name"} name={'name'}/>
+                    <S.Field required placeholder={"email"} name={'email'}/>
+                    <S.Field required placeholder={"subject"} name={'subject'}/>
+                    <S.Field required placeholder={"message"} as={"textarea"} name={'message'}/>
                     <Button type={"submit"}>Send Message</Button>
                 </S.Form>
             </Container>
